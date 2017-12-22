29b47ae367ab6c742c195b6526f0ac691cc3fad9
ElPepe <aunsoyjoven@hotmail.com>
2017-12-22 15:32:34 -0600

    Testing react-imgpro

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import Image from 'react-imgpro';
 
-import logo from '../../logo.svg';
 import Menu from '../Menu/Menu';
 import className from './Header.module.css';
 
 export default () => (
   <header className={className.header}>
     <div className="wrapper">
-      <img alt="Artesia logo" src={logo} />
+      {/* <img alt="Artesia logo" src={logo} /> */}
+      <Image
+        alt="Artesia logo"
+        image="http://365.unsplash.com/assets/paul-jarvis-9530891001e7f4ccfcef9f3d7a2afecd.jpg"
+        resize={{ width: 100, height: 50 }}
+      />
       <Menu />
     </div>
   </header>
