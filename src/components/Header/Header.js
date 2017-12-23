ec5144170e9e3f828e2423a5ce9fe0fc8e4bfe23
ElPepe <aunsoyjoven@hotmail.com>
2017-12-23 15:30:38 -0600

    Removed react-imgpro for now. Trying with react-image

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,12 +7,8 @@ import className from './Header.module.css';
 export default () => (
   <header className={className.header}>
     <div className="wrapper">
-      {/* <img alt="Artesia logo" src={logo} /> */}
-      <Image
-        alt="Artesia logo"
-        image="http://365.unsplash.com/assets/paul-jarvis-9530891001e7f4ccfcef9f3d7a2afecd.jpg"
-        resize={{ width: 100, height: 50 }}
-      />
+      {/* resize={{ width: 100, height: 50 }} */}
+      <Image alt="Artesia logo" image="/img/artesia-logo.jpg" />
       <Menu />
     </div>
   </header>
