2b94d3bf7b1798586448bae60a6ca873497eef9a
ElPepe <aunsoyjoven@hotmail.com>
2017-12-28 14:23:25 -0600

    General layout ready (mobile)

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Picture } from 'react-responsive-picture';
+import VisibilitySensor from 'react-visibility-sensor';
 // import { Link } from 'react-router-dom';
 
+import className from './Footer.module.css';
+
 export default () => (
-  <footer className="footer">
+  <footer className={className.footer}>
     <div className="wrapper">
-      <p>
-        <span>footer.</span>
-      </p>
+      <VisibilitySensor>
+        <Picture
+          className={className.logo}
+          sources={[{ srcSet: '/img/artesia-logo.jpg' }]}
+        />
+      </VisibilitySensor>
+      <p>Campos Elseso No. 200, Polanco. Ciudad de México</p>
+      <p>ventas@artesia.mx</p>
+      <p>+52 (044) 55 5280 4764</p>
+      <hr />
+      <p>Copyright Artesia 2017. Todos los derechos reservados</p>
     </div>
   </footer>
 );
