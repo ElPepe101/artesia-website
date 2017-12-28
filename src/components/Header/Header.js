02b3035742903750db959f4e84fbc76bd21bc772
ElPepe <aunsoyjoven@hotmail.com>
2017-12-28 14:59:54 -0600

    Added link to header logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import Image from 'react-image';
+import { Picture } from 'react-responsive-picture';
+import { NavLink } from 'react-router-dom';
 import MenuMobile from 'react-burger-menu/lib/menus/slide';
 
 import Menu from '../Menu/Menu';
 import Nav from '../Menu/Menu.Nav';
 import className from './Header.module.css';
-
-const error = err => console.log('err: ', err);
-const load = msg => console.log('msg: ', msg);
 
 export default () => (
   <header className={className.header}>
@@ -20,13 +18,13 @@ export default () => (
       <Nav />
     </MenuMobile>
     <div className="wrapper">
-      <Image
-        alt="Artesia logo"
-        src={['/img/artesia-logo.jpg']}
-        className="logo"
-        onLoad={load}
-        onError={error}
-      />
+      <NavLink exact to="/">
+        <Picture
+          alt="Artesia logo"
+          sources={[{ srcSet: '/img/artesia-logo.jpg' }]}
+          className="logo"
+        />
+      </NavLink>
       <Menu />
     </div>
   </header>
