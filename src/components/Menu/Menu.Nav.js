2e9983b1226390607ed399ffe7adfc75e71549a5
ElPepe <aunsoyjoven@hotmail.com>
2017-12-28 14:23:42 -0600

    Menu ready (mobile)

--- src/components/Menu/Menu.Nav.js
+++ src/components/Menu/Menu.Nav.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import className from './Menu.Nav.module.css';
+
 export default () => (
-  <ul>
-    <NavLink exact to="/" activeClassName="active">
-      Home
-    </NavLink>
+  <ul className={className.list}>
+    <li>
+      <NavLink exact to="/" activeClassName="active">
+        Conócenos
+      </NavLink>
+    </li>
+    <li>
+      <NavLink exact to="/" activeClassName="active">
+        Pisos residenciales
+      </NavLink>
+    </li>
+    <li>
+      <NavLink exact to="/" activeClassName="active">
+        Servicios
+      </NavLink>
+    </li>
+    <li>
+      <NavLink exact to="/" activeClassName="active">
+        Galería
+      </NavLink>
+    </li>
+    <li>
+      <NavLink exact to="/" activeClassName="active">
+        Ubicación
+      </NavLink>
+    </li>
+    <li>
+      <NavLink exact to="/" activeClassName="active">
+        Contáctanos
+      </NavLink>
+    </li>
   </ul>
 );
