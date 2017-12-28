59dc800b454900555d251f825e9bacf2750c85f3
ElPepe <aunsoyjoven@hotmail.com>
2017-12-28 14:23:55 -0600

    Home ready (mobile)

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,39 @@
 import React from 'react';
 import { Button } from 'react-scroll';
+import { Picture } from 'react-responsive-picture';
+import VisibilitySensor from 'react-visibility-sensor';
+
 import className from './Home.module.css';
-import { header } from '../Header/Header.module.css';
 
 export default () => (
   <div className={className.home}>
     <h1 className={className.title}>Artesia Polanco</h1>
     <section name="hero">
-      <div className="wrapper">
-        <p>
-          Ubicado en la avenida más prestigiada de todo Polanco,
-          <strong>CAMPOS ELISEOS</strong>. {header.height}
+      <VisibilitySensor>
+        <Picture
+          sources={[
+            /* {
+              srcSet: ', /img/artesia-home-16-80.jpg',
+              media: '(min-width: 375px)',
+            }, */
+            {
+              srcSet: '/img/artesia-home-17-80.jpg'
+            }
+          ]}
+        />
+      </VisibilitySensor>
+      <div className="wrapper flex-column-end">
+        <p className="color-white">
+          Ubicado en la avenida más prestigiada de todo Polanco,{' '}
+          <strong>CAMPOS ELISEOS</strong>.
         </p>
         <Button
           to="description"
           smooth
-          duration={500}
+          readOnly
+          duration={350}
           value="Va pa bajo"
-          className="button"
+          className="button color-white"
         />
       </div>
     </section>
@@ -25,7 +41,7 @@ export default () => (
       <div className="wrapper">
         <p>
           <strong>
-            Artesia Polanco. Satisfaciendo a los clientes
+            Artesia Polanco. Satisfaciendo a los clientes{' '}
             <span className="remark">
               más exigentes y sofisticados de México.
             </span>
@@ -33,14 +49,44 @@ export default () => (
         </p>
         <p>
           Ubicado en la avenida más prestigiada de Polanco, Campos Eliseos. Sólo{' '}
-          <strong>38 Pisos Residenciales</strong>
-          divididos en dos torres, piedra y cristal.
+          <strong>38 Pisos Residenciales</strong> divididos en dos torres,
+          piedra y cristal.
         </p>
         <p>
-          Desde <strong>254.36m2</strong> a <strong>641.32m2</strong>
+          Desde <strong>254.36m2</strong> a <strong>641.32m2</strong>{' '}
           satisfaciendo a los clientes más exigentes y sofisticados.
         </p>
+        <button className="button-square">Conozca los pisos</button>
       </div>
+    </section>
+    <section name="_360">
+      <VisibilitySensor>
+        <div>
+          <Picture
+            sources={[
+              {
+                srcSet: '/img/artesia-home-15-80.jpg'
+              }
+            ]}
+          />
+          <Picture
+            sources={[
+              {
+                srcSet: '/img/artesia-home-16-80.jpg'
+              }
+            ]}
+          />
+        </div>
+      </VisibilitySensor>
     </section>
   </div>
 );
+
+/*
+<h2>Artesia Polanco</h2>
+      <p>
+        Sólo <strong>38 Pisos Residenciales</strong> divididos en dos torres,{' '}
+        Piedra y Cristal. <strong>Desde 25436m2 a 641.35m2</strong> satisfaciendo
+        a los clientes más exigentes y sofisticados.
+      </p>
+*/
